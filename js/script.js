7016bdc0ea54240a9f01e1d14808e1914585cefc
blackamber1978 <150267998+bentiger1904@users.noreply.github.com>
2024-01-29 16:51:24 +0000

    Main card body

--- js/script.js
+++ js/script.js
@@ -48,3 +48,46 @@ function getPrevious() {
 		fiveDayE1();
 	});
 };
+
+//Grab the main card body.
+var cardBody = $('.todayCard')
+
+function todaysWeather() {
+	var weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`;
+	
+	$(cardBody).empty();
+
+	fetch(weatherURL)
+		.then(response => response.json())
+		.then(data => {
+			// Update city name
+			$('.todayCardName').text(data.name);
+			$('.todayCardDate').text(date);
+			//Weather emojis
+			const emojiUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+			$('.emojis').attr('src', emojiUrl);
+			
+			// Convert temperature from Fahrenheit to Celsius
+			const celsiusTemperature = ((data.main.temp - 32) * 5/9).toFixed(2);
+			// Create and append temperature element
+			const tempElement = $('<p>').text(`Temperature: ${celsiusTemperature}°C`);
+			cardBody.append(tempElement);
+			// Convert feels like from Fahrenheit to Celsius
+			const feelsLikeCelsius = ((data.main.feels_like - 32) * 5/9).toFixed(2);
+			// Create and append feels like element
+			const feelsLikeElement = $('<p>').text(`Feels like: ${feelsLikeCelsius}°C`);
+			cardBody.append(feelsLikeElement);
+
+			// Humidity
+			const humidityElement = $('<p>').text(`Humidity: ${data.main.humidity}%`);
+			cardBody.append(humidityElement);
+
+			// Wind speed
+			const windSpeedElement = $('<p>').text(`Wind speed: ${data.wind.speed} MPH`);
+			cardBody.append(windSpeedElement);
+		})
+
+		fiveDayE1();
+	};
+
+    
